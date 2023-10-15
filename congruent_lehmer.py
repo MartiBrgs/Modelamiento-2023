80fc98f40c917f55373f6e9093dd366f4e0ba11d
MartiBrgs <martinbrgs@gmail.com>
2023-10-15 13:13:07 -0300

    creacion y refector de algoritmos

---
+++ congruent_lehmer.py
@@ -0,0 +1,111 @@
+"""
+This script contains D.H Lehmer method for getting random numbers
+"""
+from pydantic import BaseModel, conint
+import numpy as np
+
+class NonPrimeRelativesNumbers(Exception):
+    def __init__(self, mensaje="The congruential algorithm needs c and m to be relatively prime numbers"):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
+
+class lehmerParams(BaseModel):
+    x0: conint(gt=0)
+    k: conint(gt=0)
+    g: conint(gt=0)
+    c: conint(gt=0)
+
+def relative_prime_checker(a:int, b:int) -> int:
+    """
+    This function check if two numbers are relative primes to each other
+    It will only check from 2 to 99 their greatest common divisor (GCD)
+    """
+    for i in range(2,100):
+        if a % i == 0 and b % i == 0:
+            return i
+    return 0
+
+def lehmer_algorithm(x0, a, c, m) -> list:
+    """
+    This function returns two lists, one with the x_n 
+    and the other one with the actual random number r_n
+    """
+
+    # initilizes two lists with zeros
+    x_list = np.zeros(m+1)
+    r_list = np.zeros(m)
+    # Add the first seed to the x list
+    x_list[0] = x0
+
+    # Calculating every x and adding it to the list
+    for i in range(len(x_list)-1):
+        x_list[i+1] = (a * x_list[i] + c) % m
+
+    # Making the list start from x1 up to xn
+    x_list = x_list[1:]
+
+    # Calculating every r_i associated with every x_i, with i=1,2,3...
+    for i in range(len(x_list)):
+        r_list[i] = x_list[i] / (m - 1)
+
+    # rounding to 4 decimals
+    r_list = np.round(r_list, decimals=4)
+
+    return x_list, r_list
+
+
+def congruentLehmer(params: lehmerParams) -> list:
+    """
+    This function creates a list of random generated numbers between 0 and 1
+    using the D.H Lehmer congruent algorithm (a linear algorithm)
+
+    Inputs are:
+    x0: an integer wich represents the initial seed for the algorithm
+    k: an integer used to define "a"
+    g: an integer to define "m=2^g", wich is the maximum life time of the algorithm (N)
+    c: an integer that should be a relative prime to "m"
+
+    Returns two lists of lenght "m" containing #m random generated numbers in list_r (floats between 0 and 1)
+    and the inter-calculations of x's.
+    """
+    # Parameters
+    x0 = params.x0
+    k = params.k
+    g = params.g
+    c = params.c
+
+    m = pow(2,g)
+    a = 1 + (4 * k)
+
+    # Checking if m and c are relatively prime numbers    
+    gcd = relative_prime_checker(m,c) # calculates gratest common divisor between m and c
+    
+    if (gcd > 1):
+        error = "El algoritmo necesita que c y m sean relativamente primos\n" + \
+                f"c={c}, m={m}, Máximo Común Divisor={gcd}"
+        raise NonPrimeRelativesNumbers(error)
+
+    # Generating the random generated numbers
+    x_list, r_list = lehmer_algorithm(x0, a, c, m)
+
+    return x_list, r_list
+
+
+# Testing only
+if __name__ == "__main__":
+    
+    test_params = {
+        "x0": 6, 
+        "k": 3, 
+        "g": 3, 
+        "c": 7
+    }
+    
+    x_list, r_list = congruentLehmer(lehmerParams(**test_params))
+
+    print("Valores para lehmer")
+    for key, value in test_params.items():
+        print(f"{key}: {value}")
+    print("resultados")
+    for i in range(len(x_list)):
+        print(f"x[{i+1}]: {x_list[i]}  --> r[{i+1}]: {r_list[i]}")
---
+++ congruent_mult.py
@@ -0,0 +1,102 @@
+"""
+This script contains congruential product method for getting random numbers
+"""
+from pydantic import BaseModel, conint, validator
+import numpy as np
+
+class NonOddSeedError(Exception):
+    def __init__(self, mensaje="The seed must be an odd number"):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
+
+# Validando datos de entrada
+class multParams(BaseModel):
+    x0: conint(gt=0)
+    k: conint(ge=0)
+    g: conint(gt=0)
+
+def odd_check(value: int) -> bool:
+    if value % 2 == 0:
+        return False
+    return True
+    
+def mult_c_algorithm(x0, a, m, N) -> list:
+    """
+    This function calculates random generated numbers with
+    the product congruential method and returns two lists
+    """
+
+    # Initialazing numpy arrays to contain the numbers generated (to work with floats)
+    x_list = np.zeros(N+1)
+    r_list = np.zeros(N)
+
+    x_list[0] = x0
+
+    # Calculating x_n values
+    for i in range(len(x_list) - 1):
+        x_list[i+1] = (a * x_list[i]) % m
+
+    x_list = x_list[1:]
+
+    # Calculating r_n values
+    for i in range(len(x_list)):
+        r_list[i] = x_list[i] / (m - 1)
+
+    # rounding to 4 decimals
+    r_list = np.round(r_list, decimals=4)
+
+    return x_list, r_list
+
+
+    
+def congruentMult(params: multParams) -> list:
+    """
+    This function creates a list of random generated numbers between 0 and 1
+    using the product congruential algorithm
+
+    Inputs are:
+    x0: an odd integer wich represents the initial seed for the algorithm
+    k: an integer used to define "a" k should be greater or equal to 0
+    g: an integer to define "m=2^g", wich is the maximum life time of the algorithm (N)
+
+    Returns two lists of lenght "m" containing #m random generated numbers in list_r (floats between 0 and 1)
+    and the inter-calculations of x's.
+    """
+    # parameters
+    x0 = params.x0
+
+    if (odd_check(x0) == False):
+        error = f"La semilla x0 debe ser impar\nValor ingresado x0: {x0}"
+        raise NonOddSeedError(error)
+    
+    k = params.k
+    g = params.g
+
+    # a = 3 + (8 * k)
+    a = 5 + (8 * k)
+    m = pow(2, g)
+    N = pow(2, g-2) # life time of the algorithm
+
+    # Obtaining random number list
+    x_list, r_list = mult_c_algorithm(x0, a, m, N)
+
+    return x_list, r_list
+
+
+# Testing only
+if __name__ == "__main__":
+    
+    test_params = {
+        "x0": 16, 
+        "k": 2, 
+        "g": 5
+    }
+    
+    x_list, r_list = congruentMult(multParams(**test_params))
+
+    print("Valores para congruencial multiplicativo")
+    for key, value in test_params.items():
+        print(f"{key}: {value}")
+    print("resultados")
+    for i in range(len(x_list)):
+        print(f"x[{i+1}]: {x_list[i]}  --> r[{i+1}]: {r_list[i]}")
