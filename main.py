65a15e3dfb170894b910d459bbba71709739ae26
Barbara <barbvergara>
2023-10-17 20:24:57 -0300

    agregue la pagina del metodo cuadrados medios

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from congruent_lehmer import congruentLehmer, lehmerParams, NonPrimeRelativesNum
 from congruent_mult import congruentMult, multParams, NonOddSeedError
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
+from cuadrados_medios import cuadMedios, cuadParams, nonDigitos
 
 app = FastAPI()
 
@@ -57,6 +58,31 @@ async def procesar_datos(
             "r_list": r_list.tolist()
         }
     except NonPrimeRelativesNumbers as e:
+        result = {"error": str(e)}
+
+    return result
+
+
+
+@app.post("/cuadM")
+async def procesar_datos(
+    d: int = Form(...),
+    x0: int = Form(...),
+    output_len: int = Form(...),
+):
+    params = {
+        "d": d,
+        "x0": x0,
+        "output_len": output_len,
+    }
+
+    try:
+        y_list, r_list = cuadMedios(cuadParams(**params))
+        result = {
+            "y_list": y_list.tolist(),
+            "r_list": r_list.tolist()
+        }
+    except nonDigitos as e:
         result = {"error": str(e)}
 
     return result
