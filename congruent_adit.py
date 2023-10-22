081444aaf4090f4b0167a17cf8fe98ca2fb14b1e
Barbara <barbvergara>
2023-10-22 19:06:18 -0300

    codigo para algoritmo congruencial aditivo

---
+++ congruent_adit.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, conint
+import numpy as np
+
+class nonDigitos(Exception):
+    def __init__(self, mensaje="The seed must have D digits"):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
+
+class Params(BaseModel):
+    n: conint(ge=1)  # Longitud de la secuencia previa
+    x_values: list  # Lista de n números enteros iniciales
+    m: conint(gt=0)  # Módulo m, debe ser un entero positivo
+    output_len: conint(gt=0)
+
+def secuenciaX(params: Params):
+    n = params.n
+    x_values = params.x_values
+    m = params.m
+    output_len = params.output_len
+
+    if len(x_values) != n:
+        raise ValueError(f"La longitud de la secuencia previa debe ser igual a n = {n}")
+
+    sequence = x_values.copy()
+
+    for i in range(n + 1, n + output_len + 1):
+        xi = (sequence[-1] + sequence[-n]) % m
+        sequence.append(xi)
+    
+    return n, sequence  # Devolver n junto con la secuencia generada
+
+def congruentAditivo(n, sequence, m):
+    r_list = [x / m for x in sequence[n:]]
+    return r_list
+
+if __name__ == "__main__":
+    test_params = {
+        "n": 5,  # Cantidad de elementos de la lista inicial 
+        "x_values": [65, 89, 98, 3, 5],  # Valores iniciales 
+        "m": 100,  
+        "output_len": 15
+    }
+
+    secuenciaX(Params(**test_params))
+
