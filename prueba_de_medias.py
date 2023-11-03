9af6137d99b70c8f0660deb87ece79024b441665
MartiBrgs <martinbrgs@gmail.com>
2023-11-03 13:05:50 -0300

    modified to return a list with rounded numbers

--- prueba_de_medias.py
+++ prueba_de_medias.py
@@ -24,27 +24,32 @@ def upperBound(n: int, confidence: float):
     z_value = norm.ppf(1 - alpha_over_2)
     return (1/2) + z_value * (1 / np.sqrt(12 * n)) 
 
-def pruebaDeMedias(confidence: float, r_list: list):
+def pruebaDeMedias(r_list: list, confidence: float=0.95):
     r_avg = promedio(r_list)
     n = len(r_list)
     LB = lowerBound(n, confidence)
     UB = upperBound(n, confidence)
 
-    return r_avg, LB, UB
+    r_avg = round(r_avg, 4)
+    LB = round(LB, 4)
+    UB = round(UB, 4)
+
+    pmedias_list = [r_avg, LB, UB]
+
+    return pmedias_list
 
 
 if __name__ == "__main__":
     # Test only
-    confidence = 0.95
     r_list = [0.4667, 0.6667, 0.6, 0., 0.2, 0.9333, 0.3333, 0.2667, 1., 0.1333 ,0.0667, 0.5333, 0.7333, 0.4, 0.8667, 0.8]
 
-    r_avg, LB, UB = pruebaDeMedias(confidence, r_list)
+    pmedias = pruebaDeMedias(r_list=r_list)
 
     print("Prueba de medias")
 
-    print(f"promedio: {r_avg}, Lower bound: {LB}, Upper bound: {UB}")
+    print(f"promedio: {pmedias[0]}, Lower bound: {pmedias[1]}, Upper bound: {pmedias[2]}")
 
-    if (r_avg >= LB and r_avg <= UB):
+    if (pmedias[0] >= pmedias[1] and pmedias[0] <= pmedias[2]):
         print("El promedio se encuentra entre los limites, no se rechaza H0")
     else:
         print("El promedio no se encuentra entre los limites, se rechaza H0")
